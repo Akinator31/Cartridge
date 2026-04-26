7f5f67d298128a68e962f878d8d7274ef36e0d21
Akinator31 <paveldewavrechin@gmail.com>
2026-04-26 14:55:25 +0200

    feat(shooter): add game menu and scoreboard scene logic

---
+++ src/games/space_shooter/scenes/scoreboard.c
@@ -0,0 +1,16 @@
+#include "shooter.h"
+#include "cartridge.h"
+#include <gb/gb.h>
+#include <gbdk/console.h>
+#include <stdio.h>
+
+void shooter_scoreboard_scene(UINT8* keys, shooter_game_st* shooter_st) {
+    (void)keys;
+    (void)shooter_st;
+    gotoxy(3, 4);
+    printf("SCOREBOARD");
+    gotoxy(2, 8);
+    printf("NO SCORES YET");
+    gotoxy(2, 14);
+    printf("SELECT = MENU");
+}
--- src/games/space_shooter/space_shooter.c
+++ src/games/space_shooter/space_shooter.c
@@ -1,14 +1,24 @@
+#include "shooter.h"
 #include "cartridge.h"
+#include <gb/gb.h>
+#include <stdlib.h>
+
+static const shooter_scene_functions_st shooter_scene_functions[] = {
+    { .scene_func = shooter_menu_scene },
+    { .scene_func = shooter_game_scene },
+    { .scene_func = shooter_scoreboard_scene }
+};
 
 shooter_game_st* load_shooter(void) {
-    // Initialize Shooter game resources
-    return NULL;
+    shooter_game_st* shooter_st = (shooter_game_st*)malloc(sizeof(shooter_game_st));
+    shooter_st->previous_keys = 0;
+    shooter_st->current_scene = SHOOTER_MENU;
+    shooter_st->score = 0;
+    return shooter_st;
 }
 
 void update_shooter(UINT8* keys, shooter_game_st* shooter_st) {
-    (void)keys;
-    (void)shooter_st;
-    // Update Shooter game logic
+    shooter_scene_functions[shooter_st->current_scene].scene_func(keys, shooter_st);
 }
 
 void unload_shooter(void) {
