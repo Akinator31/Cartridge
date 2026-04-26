a5895e8e9bf0b9c38775f47af2fbb03f0174cc5d
Akinator31 <paveldewavrechin@gmail.com>
2026-04-26 17:33:05 +0200

    refactor(cmake): add all includes

--- include/shooter/shooter.h
+++ include/shooter/shooter.h
@@ -1,7 +1,21 @@
 #pragma once
 #include <gb/gb.h>
 
+#define SHOOTER_MAX_BULLETS 8
+
 typedef enum { SHOOTER_MENU, SHOOTER_PLAY, SHOOTER_SCOREBOARD } shooter_scene;
+
+typedef struct bullet_s {
+        UINT8 active;
+        UINT8 x;
+        UINT8 y;
+} bullet_st;
+
+typedef struct enemy_s {
+        UINT8 active;
+        UINT8 x;
+        UINT8 y;
+} enemy_st;
 
 typedef struct shooter_game_s {
         UINT8 previous_keys;
@@ -9,6 +23,9 @@ typedef struct shooter_game_s {
         UINT16 score;
         UINT8 player_x;
         UINT8 player_y;
+        UINT8 fire_cooldown;
+        bullet_st bullets[SHOOTER_MAX_BULLETS];
+        enemy_st enemy;
 } shooter_game_st;
 
 typedef struct shooter_scene_functions_s {
@@ -18,3 +35,7 @@ typedef struct shooter_scene_functions_s {
 void shooter_menu_scene(UINT8* keys, shooter_game_st* shooter_st);
 void shooter_game_scene(UINT8* keys, shooter_game_st* shooter_st);
 void shooter_scoreboard_scene(UINT8* keys, shooter_game_st* shooter_st);
+
+void update_player_movement(UINT8* keys, shooter_game_st* shooter_st);
+void handle_player_fire(const UINT8* keys, shooter_game_st* shooter_st);
+void update_bullets(UINT8* keys, shooter_game_st* shooter_st);
