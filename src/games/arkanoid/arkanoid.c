19082698d43343a889f276b16d2aeb993291656c
Kevin SALANIER <kevin.salanier@epitech.eu>
2026-04-28 14:39:36 +0200

    feat(arkanoid): implement game state initialization in load_arkanoid function

--- src/games/arkanoid/arkanoid.c
+++ src/games/arkanoid/arkanoid.c
@@ -1,9 +1,21 @@
 #include "arkanoid.h"
 #include "cartridge.h"
 
+static arkanoid_st game_state;
+
 arkanoid_st* load_arkanoid(void) {
-    // Initialize Arkanoid resources
-    return NULL;
+    game_state.paddle_x = 72;
+    game_state.ball_x = 80;
+    game_state.ball_y = 100;
+    game_state.ball_dx = 1;
+    game_state.ball_dy = -1;
+    game_state.game_over = 0;
+    game_state.score = 0;
+    game_state.lives = 3;
+    game_state.current_scene = ARKANOID_MENU;
+    game_state.previous_keys = 0;
+
+    return &game_state;
 }
 
 void update_arkanoid(UINT8* keys, arkanoid_st* arkanoid_st) {
