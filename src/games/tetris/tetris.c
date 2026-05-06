2f5a5f3fc8743f5d91003733acaa42ee3c1ca3d6
Nogo4 <noah.gomes@epitech.eu>
2026-05-06 17:12:58 +0200

    feat(tetris): update draw_tetromino function to include sprite index and add show_next_tetromino function

--- src/games/tetris/tetris.c
+++ src/games/tetris/tetris.c
@@ -49,7 +49,7 @@ tetris_game_st* load_tetris(void) {
     tetris_st->next_tetromino = get_random_tetromino();
     tetris_st->can_get_next_tetromino = TRUE;
     set_sprite_data(0, 1, tetris_tile);
-    for (uint8_t i = 0; i < 4; i++)
+    for (uint8_t i = 0; i < 8; i++)
         set_sprite_tile(i, 0);
     return tetris_st;
 }
